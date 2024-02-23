8a7bd6ee0afaad5e4fab572b2823aa3ccbaebe0b
Deshraj Tiwari <deshrajtiwari690@gmail.com>
2024-02-23 15:36:34 +0530

    Resetting Defaults

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Ubuntu } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const ubuntu = Ubuntu({ subsets: ["latin"], weight: "400" });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +16,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={ubuntu.className}>
+        <main className="flex flex-col justify-center items-center min-h-screen">
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
